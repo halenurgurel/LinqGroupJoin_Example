fbb44e6d23828b03c2d22ee119c4217ef5759b67
halenurgurel <halenur.gurel@hotmail.com>
2025-06-23 14:38:54 +0300

    Add project files.

---
+++ LinqGroupJoin_Example/Program.cs
@@ -0,0 +1,41 @@
+﻿using LinqGroupJoin_Example;
+
+//Listeleri tanımladık
+var students = new List<Students>
+{
+    new(1, "Ali", 1),
+    new(2, "Ayşe", 2),
+    new(3, "Mehmet", 1),
+    new(4, "Fatma", 3),
+    new(5, "Ahmet", 2)
+};
+
+var classes = new List<Classes>
+{
+    new(1, "Matematik"),
+    new(2, "Türkçe"),
+    new(3, "Kimya")
+};
+
+//Linq sorgularını yazıyoruz.
+
+//Öğrenciler ve sınıflar arasında group join işlemi yaparak, her sınıfın altında o sınıfa ait olan öğrencilerin listelendiği bir sonuç elde edin.
+//Sonuçları sınıf adıyla birlikte, o sınıfa ait öğrencilerin isimleriyle birlikte ekrana yazdıran bir LINQ sorgusu yazın.
+
+var classesWithStudents = classes.GroupJoin(students, c => c.ClassId,
+                                                      s => s.ClassId,
+                                                      (c, studentGroup) => new
+                                                      {
+                                                          ClassName = c.ClassName,
+                                                          Students = studentGroup
+                                                      });
+
+foreach (var group in classesWithStudents)
+{
+    Console.WriteLine($"Sınıf: {group.ClassName}");
+    foreach (var student in group.Students)
+    {
+        Console.WriteLine($" - {student.StudentName}");
+    }
+    Console.WriteLine();
+}
---
+++ LinqGroupJoin_Example/Students.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqGroupJoin_Example
+{
+    public class Students
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int ClassId { get; set; }
+
+        public Students(int studentId, string studentName, int classId)
+        {
+            StudentId = studentId;
+            StudentName = studentName;
+            ClassId = classId;
+        }
+    }
+}
